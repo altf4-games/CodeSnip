0efe153f114129740fc2fd72cc7df6ed1cd55f4a
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2023-12-23 16:49:50 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+ '/public/index.html');
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
