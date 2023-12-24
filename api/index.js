0526b7e0418363bc9fd6f1b542bfbed1eb87caa5
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2023-12-24 17:57:05 +0530

    Fix?

--- api/index.js
+++ api/index.js
@@ -1,38 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-
-const { Pool } = require('pg');
 require('dotenv').config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
 
 app.use(express.json({ limit: '3mb' }));
 app.use(cors());
 
-app.get('/test', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT *');
-    const results = { 'results': (result) ? result.rows : null };
-    res.json(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
-
 app.post('/api', (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
+  res.status(200).send('Received the data.');
 })
 
-app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server started on http://localhost:${PORT}')
 })
