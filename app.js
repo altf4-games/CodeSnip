26367f7b60037caa709c6fd4ddb38d715e3cac92
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2023-12-24 16:56:11 +0530

    /api

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname+ '/public/index.html');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '3mb' }));
+
+app.get('/test', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT *');
+    const results = { 'results': (result) ? result.rows : null };
+    res.json(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('Server started on http://localhost:3000')
 })
