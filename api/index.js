375cac002b2d3e0e0ddcb9c4b2f48691e405d714
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2023-12-25 10:36:43 +0530

    /raw and /api endpoints

--- api/index.js
+++ api/index.js
@@ -1,33 +1,33 @@
 const express = require('express')
 const app = express()
 const { sql } = require("@vercel/postgres");
+const cors = require('cors');
 
 app.use(express.json({ limit: '3mb' }));
+app.use(cors());
 require('dotenv').config();
 
-const get_test_data = async () => {
-
-  const { rows } = await sql`SELECT body FROM DB WHERE id = 1;`;
+const get_test_data = async (id) => {
+  const { rows } = await sql`SELECT body FROM DB WHERE id = ${id};`;
   return { data: rows };
 };
 
-const set_test_data = async (data,id) => {
-  //await sql`CREATE TABLE IF NOT EXISTS DB (id SERIAL PRIMARY KEY, body TEXT);`;
+const set_test_data = async (id,data) => {
+  await sql`CREATE TABLE IF NOT EXISTS DB (id SERIAL PRIMARY KEY, body TEXT);`;
   await sql`INSERT INTO DB (id, body) VALUES (${id}, ${data});`;
 }
 
 app.post('/api', async (req, res) => {
   console.log(req.body);
   await set_test_data();
-  res.send('OK');
+  res.json({ status: 'ok' });
 })
 
-app.get('/1', async (req, res) => {
-  console.log(req.body);
-  await get_test_data().then((data) => {
-    console.log(data);
-    res.json(data);
-  });
+app.get(`/raw/:id`, async (req, res) => {
+  const id = req.params.id;
+  const data = await get_test_data(id);
+  console.log(data);
+  res.json(data);
 });
 
 const PORT = process.env.PORT || 3000;
